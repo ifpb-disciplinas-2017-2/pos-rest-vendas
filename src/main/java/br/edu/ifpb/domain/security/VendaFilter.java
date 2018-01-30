d49ac9532c763d7ed7c2ea8715410099d1b99c2c
Ricardo Job <sousajob@gmail.com>
2018-01-30 15:03:12 -0300

    Segurança com Base64

---
+++ src/main/java/br/edu/ifpb/domain/security/VendaFilter.java
@@ -0,0 +1,75 @@
+package br.edu.ifpb.domain.security;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.StringTokenizer;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author Ricardo Job
+ * @mail ricardo.job@ifpb.edu.br
+ * @since 30/01/2018, 13:37:49
+ */
+@Provider
+//@Priority(Priorities.AUTHENTICATION)
+//@PreMatching
+public class VendaFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        boolean contains = requestContext.getUriInfo().getPath().contains("venda");
+        if (!contains) {
+            return;
+        }
+//        requestContext.getHeaders()
+//                .forEach((k, v) -> System.out.println(k + " -> " + v));
+        String headerString = requestContext.getHeaderString("Authorization");
+
+        boolean vazio = (headerString == null || headerString.isEmpty());
+//        System.out.println("vazio = " + vazio + " " + headerString);
+        if (vazio) {
+            JsonObject add = Json.createObjectBuilder().add("msg", "header not valid").build();
+            Response response = Response
+                    .status(Response.Status.UNAUTHORIZED)
+                    .type(MediaType.APPLICATION_JSON)
+                    .entity(add)
+                    .build();
+            requestContext.abortWith(response);
+//            return;
+        } else {
+
+            String withoutBasic = headerString.replaceAll("Basic ", "");
+//        System.out.println("headerString = " + headerString);
+            //Basic YWRtaW46bWVzbWFjb2lzYQ==
+            byte[] decode = Base64.getDecoder().decode(withoutBasic);
+//        System.out.println(new String(decode));
+            String usuarioComSenha = new String(decode);
+            StringTokenizer string = new StringTokenizer(usuarioComSenha, ":");
+            String usuario = string.nextToken();
+            String senha = string.nextToken();
+//        System.out.println("usuario = " + usuario + " senha: " + senha);
+
+            boolean usarioExiste = "admin".equals(usuario) && "mesmacoisa".equals(senha);
+
+            if (!usarioExiste) {
+                JsonObject add = Json.createObjectBuilder().add("msg", "user not find").build();
+                Response response = Response
+                        .status(Response.Status.UNAUTHORIZED)
+                        .type(MediaType.APPLICATION_JSON)
+                        .entity(add)
+                        .build();
+                requestContext.abortWith(response);
+//            return;
+            }
+        }
+//        System.out.println(".,jhaklsjhfak");
+
+    }
+
+}
